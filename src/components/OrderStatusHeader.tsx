a4be427686290c78d96b66291125056338dff1a1
tajsonbg <atanasije.rankovic@gmail.com>
2024-08-01 22:16:23 +0200

    Add env.example file with description for Readme

--- src/components/OrderStatusHeader.tsx
+++ src/components/OrderStatusHeader.tsx
@@ -22,7 +22,7 @@ const OrderStatusHeader = ({ order }: Props) => {
     return `${hours}:${paddedMinutes}`;
   };
 
-  const getOrderInfo = () => {
+  const getOrderStatusInfo = () => {
     return (
       ORDER_STATUS.find((o) => o.value === order.status) || ORDER_STATUS[0]
     );
@@ -31,12 +31,12 @@ const OrderStatusHeader = ({ order }: Props) => {
   return (
     <>
       <h1 className="text-4xl font-bold tracking-tighter flex flex-col gap-5 md:flex-row md:justify-between">
-        <span> Order status: {getOrderInfo().label}</span>
+        <span> Order status: {getOrderStatusInfo().label}</span>
         <span>Expected by: {getExpededDelivery()}</span>
       </h1>
       <Progress
         className="animate-pulse"
-        value={getOrderInfo().progressValue}
+        value={getOrderStatusInfo().progressValue}
       />
     </>
   );
